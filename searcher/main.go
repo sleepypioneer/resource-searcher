09408b9389d2dfd443bd50b8e8985d3e3321f0b9
Jessica.greene <jessica0greene@gmail.com>
2019-10-04 21:24:31 +0200

    Extends analyse function

--- searcher/main.go
+++ searcher/main.go
@@ -4,13 +4,14 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"strings"
 
 	"github.com/pkg/errors"
 )
 
 type resource struct {
-	document *document
-	meta     *meta
+	Document *document `json:"document"`
+	Meta     *meta     `json:"meta"`
 }
 
 // type raw struct {
@@ -19,15 +20,29 @@ type resource struct {
 // }
 
 type meta struct {
-	httpStatus int    `json:"http_status"`
-	url        string `json : "url"`
+	HTTPStatus int    `json:"http_status"`
+	URL        string `json:"url"`
 }
 
 type document struct {
-	title     string `json: "title"`
-	content   string `json : "content"`
+	Title     string   `json:"title"`
+	Content   []string `json:"content"`
 	wordCount int
 	topics    []string
+}
+
+var (
+	keywords    = []string{"python", "webservice", "server", "Django", "list"}
+	punctuation = []string{".", ",", "!", ":", ";", "'", "/", "-", "_", "*", "&", "{", "}", "[", "]"}
+)
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 func (s *service) scrape(url string) (*resource, error) {
@@ -42,13 +57,40 @@ func (s *service) scrape(url string) (*resource, error) {
 	if err := json.Unmarshal(data, res); err != nil {
 		return nil, errors.Wrap(err, "unmarshalling JSON")
 	}
-
 	return res, nil
+}
+
+func wordCount(content []string) int {
+	wordCount := 0
+	// for each paragraph in the article we count how many words it contains.
+	for _, p := range content {
+		// remove all punction in paragraph
+		for _, punc := range punctuation {
+			p = strings.ReplaceAll(p, punc, "")
+		}
+		// split paragraph into words list
+		words := strings.Split(p, " ")
+		wordCount += len(words)
+	}
+	// return total word count for the article
+	return wordCount
+}
+
+func findTopics(content []string) (topics []string) {
+	for _, p := range content {
+		for _, keyword := range keywords {
+			if strings.Contains(p, keyword) && !stringInSlice(keyword, topics) {
+				topics = append(topics, keyword)
+			}
+		}
+	}
+	return topics
 }
 
 func analyse(doc *document) {
 	// do something with your document
-	doc.wordCount = 42
+	doc.wordCount = wordCount(doc.Content)
+	doc.topics = findTopics(doc.Content)
 }
 
 type service struct {
@@ -75,7 +117,9 @@ func main() {
 		}
 
 		// do we need an error to be returned?
-		analyse(resource.document)
+		analyse(resource.Document)
+		log.Printf("doc word count: %d", resource.Document.wordCount)
+		log.Printf("doc topics: %v", resource.Document.topics)
 
 		if err := s.store(resource); err != nil {
 			log.Printf("ERROR: %v", err)
